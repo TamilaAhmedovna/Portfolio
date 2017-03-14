cb3ee15eadb3d36eb8b8e2b8ce5da625f2cfd051
TamilaAhmedovna <ibragimova_91@bk.ru>
2017-03-14 18:49:46 +0200

    Slider, menu, footer

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// window.onload = function () {
+//     var parent = document.querySelector(".linksMain");
+//     var child = document.querySelector(".links");
+//
+//     var cWidth = child.offsetWidth;
+//     var cHeight = child.offsetHeight;
+//
+//     parent.style.width = cWidth + "px";
+//     parent.style.height = cHeight + "px";
+// }
